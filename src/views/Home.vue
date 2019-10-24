946ba2122ae76cc80abcf0eb5a41612734057b04
Josiah Aubin <josiahaubin@gmail.com>
2019-10-24 17:05:28 -0600

    Started on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="row mt-5">
+      <div class="col-12 col-md-6">
+        <h3>Who Am I?</h3>
+      </div>
+      <div class="col-12 col-md-6">
+        <h3>Education</h3>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
