e696482ff7edc668ef76907449c0b89e34725974
Josiah Aubin <josiahaubin@gmail.com>
2019-10-24 17:02:38 -0600

    Header created

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
-  <div id="app">
+  <div id="app" class="container-fluid">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="row align-items-center mt-5">
+        <div class="col-12 col-md-6">
+          <img
+            src="https://avatars1.githubusercontent.com/u/53383352?s=460v=4"
+            alt="Josiah Aubin Photo"
+            class="mb-1"
+          />
+        </div>
+        <div class="col-12 col-md-4">
+          <h1 class="navFont">Josiah Aubin</h1>
+          <h1>Software Developer</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <a href="https://github.com/josiahaubin" class="mx-2">
+            <i class="fab fa-github"></i> GitHub
+          </a>
+          <a href="https://www.linkedin.com/in/josiah-aubin/" class="mx-2">
+            <i class="fab fa-linkedin"></i> LinkedIn
+          </a>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
+  color: black;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: black;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#nav img {
+  height: 30vh;
+  width: 30vh;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px 1px;
+}
+.navFont {
+  font-size: 4em;
 }
 </style>
