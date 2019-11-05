269a7c46c44069e8b704b8db3dab8e184df51ee2
Josiah Aubin <josiahaubin@gmail.com>
2019-11-04 17:12:22 -0700

    Added project links and images, button format

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -5,39 +5,33 @@
     </div>
     <div class="col-md-4">
       <img
-        src="http://ksplumbinginc.com/wp-content/uploads/2018/03/shutterstock_611361611-555x555.jpg"
-        alt="Coming Soon Image"
+        src="https://happybloggingspot.files.wordpress.com/2016/02/wfd_imp.png"
         class="img-projects"
       />
       <h5 class="txt-font-v2">Highland Authority</h5>
       <p class="mt-1 mx-2">
         <b>Project Description:</b> Worked with a group of peers to create a social stat tracking application for the Scottish Highland Games. We implemented user roles to allow admins to change roles and allow judges to create games and input scores for athletes. Used Vue.Js on the front-end, and used Express and Node.Js for the back-end. It also implements express-sessions for authentication, bcrypt for password hashing, mongoose as an ORM and MongoDB as the database.
       </p>
-      <button type="button" class="btn btn-light">Go to Project</button>
+      <a class="btn btn-dark" href="http://highland-games.herokuapp.com/">Go to Project</a>
     </div>
     <div class="col-md-4">
       <img
-        src="http://ksplumbinginc.com/wp-content/uploads/2018/03/shutterstock_611361611-555x555.jpg"
-        alt="Coming Soon Image"
-        class="img-projects"
+        src="https://tse2.mm.bing.net/th?id=OIP.WXV7GuYCqWfp1KopPTysIAHaFu&pid=Api&P=0&w=238&h=185"
+        class="img-fluid"
       />
       <h5 class="txt-font-v2">Kanban</h5>
       <p class="mt-1 mx-2">
         <b>Project Description:</b> Worked with a partner to create an application that allows users to create boards with lists, tasks, and comments. On the front-end we utilized Vue.Js, implementing the Vuex design and Vue-Router to manage the dom. This project also utilizes Express, Node.js, authentication, bcrypt, mongoose, and MongoDB.
       </p>
-      <button type="button" class="btn btn-light">Go to Project</button>
+      <a class="btn btn-dark" href="https://craftban.herokuapp.com/">Go to Project</a>
     </div>
     <div class="col-md-4">
-      <img
-        src="http://ksplumbinginc.com/wp-content/uploads/2018/03/shutterstock_611361611-555x555.jpg"
-        alt="Coming Soon Image"
-        class="img-projects"
-      />
+      <img src="http://www.drodd.com/images15/letter-k26.jpg" class="img-projects" />
       <h5 class="txt-font-v2">Keepr</h5>
       <p class="mt-1 mx-2">
         <b>Project Description:</b> Keepr is a social network, similar to Pinterest, that allows users to visually share, and discover new interests by posting images to their own or others' collections. It utilizes a Vue.Js front-end. The server side utilizes DotNet WebApi and implements the Repository pattern to communicate with the database. Keepr also utilizes bcrypt for password hashing, dapper as an ORM and MySQL as the database.
       </p>
-      <button type="button" class="btn btn-light">Go to Project</button>
+      <a class="btn btn-dark" href="https://keeprbcw.herokuapp.com/">Go to Project</a>
     </div>
   </div>
 </template>
@@ -60,6 +54,5 @@ export default {
 .img-projects {
   height: 25vh;
   width: 25vh;
-  border-radius: 5px;
 }
 </style>
